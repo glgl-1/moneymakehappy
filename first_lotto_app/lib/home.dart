8cc5d873588573c743c44d6f8be28aa6004af9e0
LrZl <dingtti@naver.com>
2024-08-04 11:21:54 +0900

    title
    test

--- first_lotto_app/lib/home.dart
+++ first_lotto_app/lib/home.dart
@@ -10,6 +10,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'test'
+        ),
+      ),
+    );
   }
 }
